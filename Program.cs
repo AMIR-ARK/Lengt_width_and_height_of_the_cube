c4508332d57f8fc0e30da7823560d3d38fed2189
AMIR-ARK <126375003+AMIR-ARK@users.noreply.github.com>
2023-06-25 08:30:28 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the side length of the cube: ");
+        double side = double.Parse(Console.ReadLine());
+
+        double length = CalculateLength(side);
+        double width = CalculateWidth(side);
+        double height = CalculateHeight(side);
+
+        Console.WriteLine("The length of the cube is: {0}", length);
+        Console.WriteLine("The width of the cube is: {0}", width);
+        Console.WriteLine("The height of the cube is: {0}", height);
+
+        Console.ReadKey();
+    }
+
+    static double CalculateLength(double side)
+    {
+        return side;
+    }
+
+    static double CalculateWidth(double side)
+    {
+        return side;
+    }
+
+    static double CalculateHeight(double side)
+    {
+        return side;
+    }
+}
